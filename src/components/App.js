806c075cb50677058155391dc6f30bac298c3e6a
Harmeet Singh Chugga <harmeetsinghchugga@gmail.com>
2025-10-03 10:57:06 +0530

    first commit to check workflow

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import Auth from "./Auth";
+
 
 const App = () => {
   return (
+    <>
     <div>
         {/* Do not remove the main div */}
+      click on the check box to get authenticated .
+    <Auth />
+        hiii 
     </div>
+    </>
   )
 }
 
---
+++ src/components/Auth.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+
+function Auth(){
+
+
+
+    return (
+    <>
+    <div>
+        
+        You are not authenticated.
+        okk
+
+    </div>
+    </>
+)}
+
+export default Auth;
